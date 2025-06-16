8b51e3eb2b3c40a40945f97263683b22207e9427
heygautam2001 <gautamfrankestein@gmail.com>
2025-06-16 22:13:04 +0530

    Introduction to javaScript

---
+++ JavaScript/Declaration.js
@@ -0,0 +1,46 @@
+// var
+// declaration 
+var a;
+console.log(a);
+// Initialization 
+a = 10;
+console.log(a);
+// Re-Declaration 
+var a;
+console.log(a);
+// Re-Initialization
+a=30;
+console.log(a);
+
+
+// let
+
+// declaration 
+let x ;
+console.log(x);
+// Initialization 
+x = 12;
+console.log(x);
+// Re-Declaration is not possible in case of let
+// let x; 
+// Re-Initialization
+x = 90;
+console.log(x);
+
+
+
+// Const
+
+// declaration -- Not possible
+// const x;
+// Initialization -- Not possible
+// x = 23;
+// Re-Declaration -- Not possible
+// cont x;
+// Re-Initialization--- NOt possible
+// x = 78;
+
+const name = 'hello';
+console.log(name);
+
+
---
+++ JavaScript/index.js
@@ -0,0 +1,7 @@
+console.log("welcome to javaScript classes")
+console.log(10+20)
+console.log(" hello " + " Script" );
+console.log("Welcome to javaSCript from Internal Js");
+
+var sname = "Abhishek"
+console.log(sname);
