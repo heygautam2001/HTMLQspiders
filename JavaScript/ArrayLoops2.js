3b9341d08c11819c8c37031f7398f8aafb781398
heygautam2001 <gautamfrankestein@gmail.com>
2025-06-29 13:05:23 +0530

    Filter , map , reduce added

--- JavaScript/ArrayLoops2.js
+++ JavaScript/ArrayLoops2.js
@@ -1,4 +1,4 @@
-// ! ============================ !
+// ! =============Filter Method=============== !
 
 // ? case 1 : using for loops
 
@@ -39,5 +39,101 @@ console.log(result1);
  * - It will filter the data.
  * - It will crete a new array and it will store the results.
  * syntax : collectionName.filter(callbackFn);
- * CallBack function requires parameter for iteration
+ * CallBack function requires parameter for 
+ * This callback function must and should return a condition.
+ * 
+ * . if the condittion is true element should store in an array
+ * 
  */
+
+let arr = [23 , 1 , 45 , 67 , 54 , 78 , 90 , 56 , 3 , 9 , 70 , 80 , 34 , 85 ];
+
+function looper(val)
+{
+  return val > 60;
+}
+console.log(arr.filter(looper));
+
+let looper1 = (val)=>{
+       return val > 60;
+}
+
+console.log(arr.filter(looper1));
+
+ let val = arr.filter((val)=>{
+  return val > 60;
+})
+console.log(val);
+
+// !========= Map Method ========================
+// case 1 : Using for Loop
+
+let salaries = [500 , 200 , 40 , 60 , 80 , 40 , 500 , 600 , 450];
+console.log(salaries);
+
+let updatedSal = [];
+
+for(let i = 0; i < salaries.length; i++){
+  if(salaries[i] > 200)
+    updatedSal.push(salaries[i]+50);
+
+}
+
+console.log(salaries);
+console.log(updatedSal);
+
+for(let i = 0; i < salaries.length; i++){
+    updatedSal.push(salaries[i]+50);
+}
+
+console.log(updatedSal);
+
+// ? case 2 : Using for of loop
+
+let sal = [500 , 450 , 80 , 60 , 56 , 54 , 79 , 89 , 23, 76 , 88 , 99 ];
+let upSal = [];
+for (const ele of sal) {
+ 
+  upSal.push(ele+50)
+  
+}
+console.log(sal);
+console.log(upSal);
+
+// ? Case 3 : Using Map Method
+/**
+ * It helps us to iterate through an array
+ * it will not affect the original array
+ * It takes a callbackfn as a parameter.
+ * syntax : collectionName.map(callbackFn)
+ * This callbackFn should have a parameter for itreration.
+ * This should return an expression/normal code
+ */
+
+let salY = [500 , 450 , 80 , 60 , 56 , 54 , 79 , 89 , 23, 76 , 88 , 99 ];
+let upSalY = [];
+function increment(i){
+  return i+50;
+}
+
+let upSaly = salY.map((increment));
+console.log("upsaly : ",upSaly);
+
+console.log(salY.map((ele)=>
+  {
+    return ele+200;
+  })
+
+);
+
+// ! ========== Reduce ========== !
+// syntax : collectionName.reduce(callbackFn(ele , initialVal),accumaulator)
+let arr1 = [45 , 60 , 12 , 43 , 23 , 78 , 90];
+let initialValue = 0;
+let sum = arr1.reduce((accumaulator,ele)=>{
+  console.log("accumulator : ",accumaulator);
+  console.log("ele : ",ele);
+  return accumaulator += ele;
+});
+
+console.log("Total array sum of ", arr1 , " is ", sum);
