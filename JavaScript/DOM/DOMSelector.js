f05722ab470630951c953e86f20c599d00a9df99
heygautam2001 <gautamfrankestein@gmail.com>
2025-07-01 21:30:14 +0530

    Dom updates

--- JavaScript/DOM/DOMSelector.js
+++ JavaScript/DOM/DOMSelector.js
@@ -1,8 +1,29 @@
 // ! ..................id Selector...................
 
+console.log(document);
 
+let heading3 = document.getElementById('heading3');
+console.log(heading3);
+
+let heading4 = document.getElementById('heading4');
+console.log(heading4);
+
+let heading5 = document.getElementById('heading5');
+console.log(heading5);
+
+// ! ------------- class selector -----------------
+
+let demo = document.getElementsByClassName('demo');
+console.log(demo);
+
+let demo1 = document.getElementsByClassName('demo1');
+console.log(demo1);
+
+let demo2 = document.getElementsByClassName('demo2');
+console.log(demo2);
 
 // ! Tag Name
+
 let b = document.getElementsByTagName("b");
 console.log(b);
 
@@ -23,5 +44,6 @@ console.log(userNames[0].placeholder);
 console.log(userNames[0].value);
 
 userNames[0].value = "Gautam"
+console.log(userNames[0].value);
 
 
