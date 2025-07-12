29f5ca74b8d4614a72a572bda5067138c0d6caf7
heygautam2001 <gautamfrankestein@gmail.com>
2025-07-12 12:06:30 +0530

    Array New Method added

---
+++ JavaScript/Arrays1.js
@@ -0,0 +1,45 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()//convert the array into the string
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr); //A  [ 0, 1, 2, 3, 4, 5 ]
+
+const myn1 = myArr.slice(1, 3)//last index of range not included..
+//No effect on array , array remains same.
+
+console.log(myn1); //[ 1, 2 ]
+console.log("B ", myArr);//B  [ 0, 1, 2, 3, 4, 5 ]
+
+
+const myn2 = myArr.splice(1, 3)//splice includes the last range & it also effects the original  array..
+//Changes are applied in origina array too..
+console.log("C ", myArr); //C  [ 0, 4, 5 ]
+console.log(myn2);//[ 1, 2, 3 ]
+
+// let str = "Gautam"
+// console.log(str.slice(1,5));
+
---
+++ JavaScript/date.js
@@ -0,0 +1,58 @@
+let date = new Date();
+console.log(date);
+console.log(date.toLocaleString());
+console.log(date.toLocaleTimeString());
+console.log(date.toLocaleDateString());
+
+// ! ============================================================ !
+
+ console.log(date.getFullYear());
+ console.log(date.getMonth());
+ console.log(date.getDay());
+ console.log(date.getHours());
+ console.log(date.getMinutes());
+ console.log(date.getSeconds());
+ console.log(date.getMilliseconds());
+
+ // ! ============================================================ !
+
+console.log(date.getTimezoneOffset());
+console.log(date.getUTCDate())
+console.log(date.getUTCDay())
+console.log(date.getUTCFullYear());
+console.log(date.getUTCMonth());
+console.log();
+
+// ! ============================================================= !
+
+let date1 = new Date();
+
+date1.setFullYear(2026 , 11 , 23 );
+console.log(date1.toLocaleDateString());
+
+date1.setDate(30);
+date1.setMonth(8);
+
+console.log(date1.toLocaleDateString());
+
+date1.setFullYear(2001 , 8 , 30);
+console.log(date1.toLocaleDateString());
+console.log(date1.toLocaleTimeString());
+console.log(date1.toLocaleString());
+
+
+// ^ ========================================================= ^
+// ! create your own date....
+
+let date2 = new Date(2001,8,30,11,30,53,10);
+console.log(date2.toLocaleString());
+console.log(typeof date2);
+
+// ^ ===========================================================^
+
+let date3 = new Date();
+console.log(date3.toLocaleTimeString());
+
+console.log(Date.now());
+
+console.log(date3.getTime());
